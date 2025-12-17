30191c15b5d49e3acd534709d6379bd22533c550
nissfdn <hayrunnisa9223@gmail.com>
2025-12-17 15:00:25 +0300

    duzenlenicek tekrar

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";   // 127.0.0.1 yerine localhost yazmak daha yaygın
-$user = "root";        // phpMyAdmin'de görünen kullanıcı
-$pass = "";             // XAMPP default, şifre yok
-$db   = "boyner_project"; // URL'de gördüğümüz DB adı
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "boyner_project";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$mysqli = new mysqli($host, $user, $pass, $db);
 
-if (!$conn) {
-    die("Baglanti hatasi: " . mysqli_connect_error());
+if ($mysqli->connect_error) {
+    die("Bağlantı hatası: " . $mysqli->connect_error);
 }
 ?>
