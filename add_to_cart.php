0b38c3e868dc1f96792e0772cd9fad4f6dcb359e
defne <defneozkan52@gmail.com>
2026-01-10 23:44:10 +0300

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -5,30 +5,42 @@ if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
-// Güvenli veri alma
-$id     = $_POST['product_id']    ?? null;
-$brand  = $_POST['product_brand'] ?? 'BOYNER';
-$name   = $_POST['product_name']  ?? '';
-$price  = isset($_POST['product_price']) 
+// Güvenli veri alma (Formdan gelen renk ve bedeni de ekledik)
+$id      = $_POST['product_id']    ?? null;
+$brand   = $_POST['product_brand'] ?? 'BOYNER';
+$name    = $_POST['product_name']  ?? '';
+$price   = isset($_POST['product_price']) 
             ? floatval(str_replace(",", ".", $_POST['product_price'])) 
             : 0;
-$image  = $_POST['product_image'] ?? 'no-image.webp';
+$image   = $_POST['product_image'] ?? 'no-image.webp';
 
-// Ürün ID yoksa çık
+// --- YENİ EKLENEN KISIMLAR ---
+$color   = $_POST['product_color'] ?? 'Varsayılan'; // Formdaki renk name'i neyse onu yazın
+$size    = $_POST['product_size']  ?? 'Standart';  // Formdaki beden name'i neyse onu yazın
+
+// Benzersiz anahtar oluşturuyoruz (ID + Renk + Beden)
+// Bu sayede "123_Mavi_3Yaş" ile "123_Pembe_3Yaş" ayrı satırlar olur.
+$cart_key = $id . "_" . $color . "_" . $size;
+// ----------------------------
+
 if (!$id) {
     header("Location: products.php");
     exit;
 }
 
-// Sepette varsa artır
-if (isset($_SESSION['cart'][$id])) {
-    $_SESSION['cart'][$id]['qty']++;
+// Sepette tam olarak bu varyasyon (anahtar) varsa miktar artır
+if (isset($_SESSION['cart'][$cart_key])) {
+    $_SESSION['cart'][$cart_key]['qty']++;
 } else {
-    $_SESSION['cart'][$id] = [
+    // Yoksa yeni bir satır olarak ekle
+    $_SESSION['cart'][$cart_key] = [
+        "id"    => $id,
         "brand" => $brand,
         "name"  => $name,
         "price" => $price,
         "image" => $image,
+        "color" => $color, // Rengi kaydet
+        "size"  => $size,  // Bedeni kaydet
         "qty"   => 1
     ];
 }
